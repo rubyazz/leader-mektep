72ec36ade00fb194322273ab7d8aff15ea947bf6
rubyazz <ersultan.abduvalov@gmail.com>
2023-03-31 16:21:29 +0600

    templates

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,25 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('teachers/', views.teacher_list, name='teacher_list'),
+    path('', views.index, name='index'),
+    path('tea/', views.teacher_list, name='teacher_list'),
+    path('activities/', views.activities, name='activities'),
+    path('pupils/', views.pupils, name='pupils'),
+    path('kundylyktar/', views.kundylyktar, name='kundylyktar'),
+    path('kirispe/', views.kirispe, name='kirispe'),
+    path('tarbie_zhumysy/', views.tarbie_zhumysy, name='tarbie_zhumysy'),
+    path('kuzhat/', views.kuzhat, name='kuzhat'),
+    path('tarbie_zhumystary2/', views.tarbie_zhumystary2, name='tarbie_zhumystary2'),
+    path('akimshilik/', views.akimshilik, name='akimshilik'),
+    path('suraktar/', views.suraktar, name='suraktar'),
+    path('akeler/', views.akeler, name='akeler'),
+    path('oob-zhumys/', views.oob_zhumys, name='oob_zhumys'),
+    path('ata-ana/', views.ata_ana, name='ata_ana'),
+    path('kuntizbe/', views.kuntizbe, name='kuntizbe'),
+    path('ata-ana-kom/', views.ata_ana_kom, name='ata_ana_kom'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('bailanys/', views.bailanys, name='bailanys'),
+    path('forma/', views.forma, name='forma'),
+    path('teachers/', views.teachers, name='teachers'),
+    path('uirme/', views.uirme, name='uirme'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -4,4 +4,86 @@ from .models import Teacher
 
 def teacher_list(request):
     teachers = Teacher.objects.all()
-    return render(request, 'teachers.html', {'teachers': teachers})
+    return render(request, 'tea.html', {'teachers': teachers})
+
+# return func of templates
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def activities(request):
+    return render(request, 'activities.html')
+
+
+def pupils(request):
+    return render(request, 'pupils.html')
+
+
+def kundylyktar(request):
+    return render(request, 'kundylyktar.html')
+
+
+def kirispe(request):
+    return render(request, 'kirispe.html')
+
+
+def tarbie_zhumysy(request):
+    return render(request, 'tarbie_zhumysy.html')
+
+
+def kuzhat(request):
+    return render(request, 'kuzhat.html')
+
+
+def tarbie_zhumystary2(request):
+    return render(request, 'tarbie_zhumystary2.html')
+
+
+def akimshilik(request):
+    return render(request, 'akimshilik.html')
+
+
+def suraktar(request):
+    return render(request, 'suraktar.html')
+
+
+def akeler(request):
+    return render(request, 'akeler.html')
+
+
+def oob_zhumys(request):
+    return render(request, 'oob-zhumys.html')
+
+
+def ata_ana(request):
+    return render(request, 'ata-ana.html')
+
+
+def kuntizbe(request):
+    return render(request, 'kuntizbe.html')
+
+
+def ata_ana_kom(request):
+    return render(request, 'ata-ana-kom.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def bailanys(request):
+    return render(request, 'bailanys.html')
+
+
+def forma(request):
+    return render(request, 'forma.html')
+
+
+def teachers(request):
+    return render(request, 'teachers.html')
+
+
+def uirme(request):
+    return render(request, 'uirme.html')
+
